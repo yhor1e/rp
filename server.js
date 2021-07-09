61e37de7c718773cc842310d838b5efa6b9ccb0c
yhorie <yuk.horie@gmail.com>
2021-07-09 21:30:16 +0900

    Add express as custom server

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const next = require("next");
+const path = require("path");
+
+const port = parseInt(process.env.PORT, 10) || 3000;
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  const server = express();
+
+  server.use(express.static(path.join(__dirname, "public")));
+  server.all("*", (req, res) => {
+    console.log("dddd");
+    return handle(req, res);
+  });
+
+  server.listen(port, (err) => {
+    if (err) throw err;
+    console.log(`> Ready on http://localhost:${port}`);
+  });
+});
